f17d482ba050874dc13b568c89d3837702588cd1
Andy Herbert <andy.herbert@gmail.com>
2013-11-07 21:30:43 +0000

    Add a loading spinner.

--- chrome/content/ansilove.js
+++ chrome/content/ansilove.js
@@ -1817,7 +1817,7 @@
             }
         }
 
-        divOverlay = createDiv({"position": "fixed", "left": "0px", "top": "0px", "width": "100%", "height": "100%", "backgroundColor": "rgba(0, 0, 0, 0.8)", "overflow": "scroll", "zIndex": (findHighestZIndex() + 1).toString(10), "opacity": "0"});
+        divOverlay = createDiv({"position": "fixed", "left": "0px", "top": "0px", "width": "100%", "height": "100%", "backgroundColor": "rgba(0, 0, 0, 0.8)", "overflow": "scroll", "zIndex": (findHighestZIndex() + 1).toString(10), "opacity": "0", "backgroundImage": "url(chrome://ansilove/content/spinner.gif)", "backgroundPosition": "center center", "backgroundRepeat": "no-repeat"});
         divCanvasContainer = createDiv({"backgroundColor": "black", "boxShadow": "0 8px 32px rgb(0, 0, 0)", "margin": "8px auto", "padding": "16px", "border": "4px solid white", "borderRadius": "32px", "top": "100%", "opacity": "0"});
         divOverlay.appendChild(divCanvasContainer);
         document.body.appendChild(divOverlay);
@@ -1832,6 +1832,7 @@
                     canvas.style.imageRendering = "-moz-crisp-edges";
                     canvas.style.imageRendering = "-webkit-optimize-contrast";
                     divCanvasContainer.appendChild(canvas);
+                    divOverlay.style.backgroundImage = "none";
                     transitionCSS(divCanvasContainer, "top, opacity", "0.6s, 1s", "ease-in-out, ease-out", {"top": "0", "opacity": "1"});
                     setTimeout(function () {
                         controller.play(28800);
@@ -1854,6 +1855,7 @@
                         canvas.style.imageRendering = "-webkit-optimize-contrast";
                         divCanvasContainer.appendChild(canvas);
                     });
+                    divOverlay.style.backgroundImage = "none";
                     transitionCSS(divCanvasContainer, "top, opacity", "0.6s, 1s", "ease-in-out, ease-out", {"top": "0", "opacity": "1"});
                     divOverlay.onclick = function (evt) {
                         evt.preventDefault();
