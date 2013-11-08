eb2a95ab82fc8ef01cce001de1cd34d072ac4b68
Andy Herbert <andy.herbert@gmail.com>
2013-11-08 16:51:37 +0000

    Refactored to make dropping in an updated ansilove.js script easier.

--- chrome/content/ansilove-extension.js
+++ chrome/content/ansilove-extension.js
@@ -1,36 +1,46 @@
 var ansiloveExtension = (function () {
     "use strict";
 
-    function alreadyRunning() {
-        var scripts, i;
+    function appendScript(src, callback, fail) {
+        var scripts, script, i;
+
         scripts = content.document.getElementsByTagName("script");
+
         if (scripts.length) {
             for (i = 0; i < scripts.length; ++i) {
-                if (scripts[i].src === "chrome://ansilove/content/ansilove.js") {
-                    return true;
+                if (scripts[i].src === src) {
+                    fail();
+                    return;
                 }
             }
         }
-        return false;
+
+        script = content.document.createElement("script");
+        script.type = "text/javascript";
+        script.src = src;
+        script.onload = callback;
+
+        content.document.body.appendChild(script);
     }
 
     function filterLinks() {
-        var notification, script;
+        var notification;
+
+        notification = gBrowser.getNotificationBox();
 
         function notify(text) {
             notification.appendNotification(text, "ansilove-notification", "chrome://ansilove/content/ansilove-icon.png", notification.PRIORITY_INFO_LOW, []);
         }
 
-        notification = gBrowser.getNotificationBox();
-        if (alreadyRunning()) {
+        function alreadyLoaded() {
             notify("AnsiLove is already loaded. Shift-click on any link to preview the image.");
-        } else {
-            notify("Shift-click on any link to preview with AnsiLove. Alt-click to emulate a video terminal display.");
-            script = content.document.createElement("script");
-            script.setAttribute("type", "text/javascript");
-            script.setAttribute("src", "chrome://ansilove/content/ansilove.js");
-            content.document.body.appendChild(script);
         }
+
+        appendScript("chrome://ansilove/content/ansilove.js", function () {
+            appendScript("chrome://ansilove/content/ansilove-helper.js", function () {
+                notify("Press Shift whilst clicking on any link to preview with AnsiLove; use the Alt or Option key whilst clicking to emulate a real display.");
+            }, alreadyLoaded);
+        }, alreadyLoaded);
     }
 
     return {
